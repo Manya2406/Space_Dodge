2c33af54c7248a500ff78af7c89bbaeea1e9058d
Manya2406 <20bec062@iiitdmj.ac.in>
2022-05-22 20:20:49 +0530

    game functional

--- pygame space/game.py
+++ pygame space/game.py
@@ -1,26 +1,35 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time
+    score_surf = test_font.render('Score:'+ str(int(current_time/1000)), True,(64,64,64))
+    score_rect = score_surf.get_rect(topleft = (20,10))
+    screen.blit(score_surf, score_rect)
+    
 
 pygame.init()
 screen = pygame.display.set_mode((800,468))
 pygame.display.set_caption("sprinter")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font\Pixeltype.ttf", 30)
+game_active = True
+start_time = 0
 
-scr = '0'
 
 sky_surface = pygame.image.load("2824536_1.png").convert()
-text_surface_1 = test_font.render('Score:', False, 'Black' )
-text_rect_1 = text_surface_1.get_rect(topleft = (20,10))
-text_surface_2 = test_font.render(scr, False, 'Black' )
-text_rect_2 = text_surface_2.get_rect(topleft = (90,10))
+text_surf_open = test_font.render("Game Over",False, 'White')
+text_rect_open = text_surf_open.get_rect(center = (390,50))
+text_surf_open_1 = test_font.render("Press ESCAPE KEY to Restart",False, 'White')
+text_rect_open_1 = text_surf_open_1.get_rect(center = (390,100))
+
 
 snail_surface = pygame.image.load("asteroid.png").convert_alpha()
-snail_rect = snail_surface.get_rect(bottomright = (700,380))
+snail_rect = snail_surface.get_rect(bottomright = (700,400)) 
 
 player_surf = pygame.image.load("player.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,320))
+player_gravity = -10
 
 
 while True:
@@ -28,20 +37,54 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
- #       if event.type == pygame.MOUSEMOTION:
-#            print(event.pos)
-    screen.blit(sky_surface, (0,0))   
-    pygame.draw.rect(screen, "lightblue", pygame.Rect(0,0,800,25))
-    pygame.draw.line(screen, 'Black', (0,25), (800,25),2)
-    screen.blit(text_surface_1, text_rect_1) 
-    snail_rect.x  -= 4
-    if snail_rect.right <= 0:
-        snail_rect.left = 800
-        scr = str(int(scr)+1)
-    screen.blit(snail_surface, snail_rect)
-    text_surface_2 = test_font.render(scr, False, 'Black' )
-    screen.blit(text_surface_2, text_rect_2) 
-    screen.blit(player_surf, player_rect)
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -10
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -10
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                game_active = True
+                snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
+                
+    if game_active:            
+        screen.blit(sky_surface, (0,0))   
+        pygame.draw.rect(screen, "lightblue", pygame.Rect(0,0,800,25))
+        pygame.draw.line(screen, 'Black', (0,25), (800,25),2)
+        snail_rect.x  -= 5
+        if snail_rect.right <= 0:
+            snail_rect.left = 800
+          #  scr = str(int(scr)+1)
+        screen.blit(snail_surface, snail_rect)
+#        text_surface_2 = test_font.render(scr, False, 'Black' )
+   #     screen.blit(text_surface_2, text_rect_2) 
+        display_score()
+        
+        
+        player_gravity += 0.5
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 468: player_rect.bottom = 468
+        screen.blit(player_surf, player_rect)
+        if player_rect.bottom <= 0: 
+            player_rect.bottom = 300
+        
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    
+    else:
+        screen.blit(sky_surface, (0,0)) 
+        screen.blit(text_surf_open, text_rect_open)
+        screen.blit(text_surf_open_1, text_rect_open_1) 
+        
+        
+        
+    
+ #   keys = pygame.key.get_pressed()
+  #  if keys[pygame.K_SPACE]:
+        
     
 #    mouse_pos = pygame.mouse.get_pos()
     
